c1ea1b9413a2fff58422075ab14822196481af6b
Yoav Luft <yoav.luft@prototyp.se>
2020-12-01 12:08:37 +0100

    Kinda working example it has issues

---
+++ Assets/TrackingBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class TrackingBehaviour : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var newImage in args.added)
+        {
+            Debug.Log($"New tracked images{newImage.name}");
+        }
+    }
+}
