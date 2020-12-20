9adb12044a2a07da886fa58b199adff9b45da60e
Cecilia Xia <ceciliaxia118@gmail.com>
2020-12-20 18:17:15 +0100

    Bubble burst on collision

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public GameObject bubbleBurstPrefab;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Cube")
+        {
+            if (bubbleBurstPrefab)
+            {
+                GameObject explosion = (GameObject)Instantiate(bubbleBurstPrefab, transform.position, bubbleBurstPrefab.transform.rotation);
+                Destroy(explosion, explosion.GetComponent<ParticleSystem>().main.startLifetimeMultiplier);
+                Debug.Log("Pop");
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/DragWithMouse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragWithMouse : MonoBehaviour
+{
+
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMousePos();
+    }
+
+
+
+    private Vector3 GetMousePos()
+    {
+
+        // Pixel coordinates of mouse (x,y)
+        Vector3 mousePos = Input.mousePosition;
+
+        // z coordinate of game object on screen
+        mousePos.z = mZCoord;
+
+        // Convert it to world points
+        return Camera.main.ScreenToWorldPoint(mousePos);
+
+    }
+
+
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMousePos() + mOffset;
+    }
+}
