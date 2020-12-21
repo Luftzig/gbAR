8d1c72a9b2df7d4be471f9ffd5aca16f74812f76
Cecilia Xia <ceciliaxia118@gmail.com>
2020-12-21 22:18:47 +0100

    Bubble Emitter

--- Assets/Scripts/BubbleEmitter.cs
+++ Assets/Scripts/BubbleEmitter.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class BubbleEmitter : MonoBehaviour
 {
+    public GameObject bubblePrefab;
+    public float timeSpan = 1f;
+    public int numOfBubbles = 5;
     // Emit bubbles randomly in the scene
+    void Start()
+    {
+        StartCoroutine(EmittBubble());
+    }
+    private IEnumerator EmittBubble()
+    {
+        for(int i=0; i<numOfBubbles;i++)
+        {
+            yield return new WaitForSeconds(timeSpan);
+
+            GameObject bubbleInstance = (GameObject)Instantiate(bubblePrefab);
+            bubbleInstance.transform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(0f, 3f), 0);
+        }
+    }
 }
