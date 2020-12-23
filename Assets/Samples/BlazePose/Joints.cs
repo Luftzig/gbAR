049d610f9574e8075b9bb6cd1841fb46027f0a95
Yoav Luft <yoav.luft@prototyp.se>
2020-12-23 10:37:57 +0100

    Getting pose estimation code to compile

---
+++ Assets/Samples/BlazePose/Joints.cs
@@ -0,0 +1,39 @@
+namespace TensorFlowLite
+{
+    public enum Joints
+    {
+        Nose = 0,
+        RightEyeInner = 1,
+        RightEye = 2,
+        RightEyeOuter,
+        LeftEyeInner,
+        LeftEye,
+        LeftEyeOuter,
+        RightEar,
+        LeftEar,
+        MouthRight,
+        MouthLeft,
+        RightShoulder,
+        LeftShoulder,
+        RightElbow,
+        LeftElbow,
+        RightWrist,
+        LeftWrist,
+        RightPinkyKnuckle1,
+        LeftPinkyKnuckle1,
+        RightIndexKnuckle1,
+        LeftIndexKnuckle1,
+        RightThumbKnuckle2,
+        LeftThumbKnuckle2,
+        RightHip,
+        LeftHip,
+        RightKnee,
+        LeftKnee,
+        RightAnkle,
+        LeftAnkle,
+        RightHeel,
+        LeftHeel,
+        RightFootIndex,
+        LeftFootIndex
+    }
+}
