a58d7a08a44be198c962af48f1622e89006a70c6
Cecilia Xia <ceciliaxia118@gmail.com>
2020-12-28 16:33:59 +0100

    Fruit prefabs

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public string fruitType;
+
+    void Start()
+    {
+        fruitType = this.gameObject.name;
+    }
+
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up, 20 * Time.deltaTime);
+    }
+}
