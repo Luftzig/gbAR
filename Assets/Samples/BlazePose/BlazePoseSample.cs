666e73c0f837226d6cbd9ba65f28599970c69ad8
Yoav Luft <yoav.luft@prototyp.se>
2021-01-08 15:30:38 +0100

    WIP Integrating between AR camera and TFLite vision algos

--- Assets/Samples/BlazePose/BlazePoseSample.cs
+++ Assets/Samples/BlazePose/BlazePoseSample.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TensorFlowLite;
+using UnityEngine.XR.ARFoundation;
 
 /// <summary>
 /// BlazePose form MediaPipe
@@ -18,15 +20,20 @@ public sealed class BlazePoseSample : MonoBehaviour
         FullBody,
     }
 
-    [SerializeField, FilePopup("*.tflite")] string poseDetectionModelFile = "coco_ssd_mobilenet_quant.tflite";
-    [SerializeField, FilePopup("*.tflite")] string poseLandmarkModelFile = "coco_ssd_mobilenet_quant.tflite";
+    [SerializeField, FilePopup("*.tflite")]
+    string poseDetectionModelFile = "coco_ssd_mobilenet_quant.tflite";
+
+    [SerializeField, FilePopup("*.tflite")]
+    string poseLandmarkModelFile = "coco_ssd_mobilenet_quant.tflite";
+
     [SerializeField] Mode mode = Mode.UpperBody;
-    [SerializeField] RawImage cameraView = null;
-    [SerializeField] RawImage debugView = null;
+    [SerializeField] RawImage cameraView;
+    [SerializeField] RawImage debugView;
     [SerializeField] bool useLandmarkFilter = true;
     [SerializeField, Range(2f, 30f)] float filterVelocityScale = 10;
+    [SerializeField] private Text debugText;
 
-    WebCamTexture webcamTexture;
+    Texture2D webcamTexture;
     PoseDetect poseDetect;
     PoseLandmarkDetect poseLandmark;
 
@@ -54,24 +61,33 @@ public sealed class BlazePoseSample : MonoBehaviour
         }
 
         // Init camera 
-        string cameraName = WebCamUtil.FindName(new WebCamUtil.PreferSpec()
-        {
-            isFrontFacing = false,
-            kind = WebCamKind.WideAngle,
-        });
-        webcamTexture = new WebCamTexture(cameraName, 1280, 720, 30);
-        cameraView.texture = webcamTexture;
-        webcamTexture.Play();
-        Debug.Log($"Starting camera: {cameraName}");
-
+        // string cameraName = WebCamUtil.FindName(new WebCamUtil.PreferSpec()
+        // {
+        //     isFrontFacing = false,
+        //     kind = WebCamKind.WideAngle,
+        // });
+        // webcamTexture = new WebCamTexture(cameraName, 1280, 720, 30);
+        // cameraView.texture = webcamTexture;
+        // webcamTexture.Play();
 
         draw = new PrimitiveDraw(Camera.main, gameObject.layer);
         worldJoints = new Vector3[poseLandmark.JointCount];
+        ARCameraManager arCameraManager = transform.parent.GetComponentInChildren<ARCameraManager>();
+        Debug.Assert(arCameraManager != null);
+        arCameraManager.frameReceived += OnFrameReceived;
+    }
+
+    private void OnFrameReceived(ARCameraFrameEventArgs frameEvent)
+    {
+        var texture = frameEvent.textures.First();
+        webcamTexture = texture;
+        // cameraView.texture = texture;
+        // debugText.text = $"Texture: {texture.width}x{texture.height}";
     }
 
     void OnDestroy()
     {
-        webcamTexture?.Stop();
+        // webcamTexture.Stop();
         poseDetect?.Dispose();
         poseLandmark?.Dispose();
         draw?.Dispose();
@@ -81,10 +97,15 @@ public sealed class BlazePoseSample : MonoBehaviour
     {
         poseDetect.Invoke(webcamTexture);
         cameraView.material = poseDetect.transformMat;
+        cameraView.texture = webcamTexture;
         cameraView.rectTransform.GetWorldCorners(rtCorners);
 
         var pose = poseDetect.GetResults(0.7f, 0.3f);
-        if (pose.score < 0) return;
+        Debug.Log($"BlazePose: pose.score = {pose.score}");
+        if (pose.score < 0)
+        {
+            return;
+        }
 
         DrawFrame(ref pose);
 
@@ -95,9 +116,22 @@ public sealed class BlazePoseSample : MonoBehaviour
         {
             poseLandmark.FilterVelocityScale = filterVelocityScale;
         }
+
         var landmarkResult = poseLandmark.GetResult(useLandmarkFilter);
 
         if (landmarkResult.score < 0.2f) return;
+
+        if (debugText != null)
+        {
+            Vector3 rightWrist = landmarkResult.joints[(int) Joints.JointRightWrist];
+            Vector3 leftWrist = landmarkResult.joints[(int) Joints.JointLeftWrist];
+            debugText.text = $"Wrists R: {rightWrist.ToString()}; L: {leftWrist.ToString()}";
+        }
+        else
+        {
+            Debug.LogError("No debug output found");
+        }
+
 
         DrawCropMatrix(poseLandmark.CropMatrix);
         DrawJoints(landmarkResult.joints);
@@ -105,6 +139,7 @@ public sealed class BlazePoseSample : MonoBehaviour
 
     void DrawFrame(ref PoseDetect.Result pose)
     {
+        Debug.Log("BlazePose: DrawFrame()");
         Vector3 min = rtCorners[0];
         Vector3 max = rtCorners[2];
 
@@ -113,8 +148,9 @@ public sealed class BlazePoseSample : MonoBehaviour
 
         foreach (var kp in pose.keypoints)
         {
-            draw.Point(MathTF.Lerp(min, max, (Vector3)kp, true), 0.05f);
+            draw.Point(MathTF.Lerp(min, max, (Vector3) kp, true), 0.05f);
         }
+
         draw.Apply();
     }
 
@@ -125,8 +161,9 @@ public sealed class BlazePoseSample : MonoBehaviour
         Vector3 min = rtCorners[0];
         Vector3 max = rtCorners[2];
 
-        var mtx = WebCamUtil.GetMatrix(-webcamTexture.videoRotationAngle, false, webcamTexture.videoVerticallyMirrored)
-            * matrix.inverse;
+        // var mtx = WebCamUtil.GetMatrix(-webcamTexture.videoRotationAngle, false, webcamTexture.videoVerticallyMirrored)
+        // * matrix.inverse;
+        var mtx = Matrix4x4.identity;
         Vector3 a = MathTF.LerpUnclamped(min, max, mtx.MultiplyPoint3x4(new Vector3(0, 0, 0)));
         Vector3 b = MathTF.LerpUnclamped(min, max, mtx.MultiplyPoint3x4(new Vector3(1, 0, 0)));
         Vector3 c = MathTF.LerpUnclamped(min, max, mtx.MultiplyPoint3x4(new Vector3(1, 1, 0)));
@@ -139,7 +176,9 @@ public sealed class BlazePoseSample : MonoBehaviour
     void DrawJoints(Vector3[] joints)
     {
         // Apply webcam rotation to draw landmarks correctly
-        Matrix4x4 mtx = WebCamUtil.GetMatrix(-webcamTexture.videoRotationAngle, false, webcamTexture.videoVerticallyMirrored);
+        // Matrix4x4 mtx = WebCamUtil.GetMatrix(-webcamTexture.videoRotationAngle, false,
+        //     webcamTexture.videoVerticallyMirrored);
+        Matrix4x4 mtx = Matrix4x4.identity;
         Vector3 min = rtCorners[0];
         Vector3 max = rtCorners[2];
 
@@ -158,6 +197,7 @@ public sealed class BlazePoseSample : MonoBehaviour
         {
             draw.Cube(worldJoints[i], 0.2f);
         }
+
         var connections = poseLandmark.Connections;
         for (int i = 0; i < connections.Length; i += 2)
         {
@@ -166,6 +206,7 @@ public sealed class BlazePoseSample : MonoBehaviour
                 worldJoints[connections[i + 1]],
                 0.05f);
         }
+
         draw.Apply();
     }
 }
