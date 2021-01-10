c4a107f0f04d91400fff927c6c464bb9564a0e54
Yoav Luft <yoav.luft@prototyp.se>
2021-01-10 18:40:17 +0100

    Position the bubble emitter in the center of the largest surface

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class GameManager : MonoBehaviour
+{
+    private GameState gameState;
+    public ARPlaneManager arPlaneManager;
+    public ARRaycastManager arRaycastManager;
+    public Text textField;
+    public float areaThreshold = 1.5f * 1.5f;
+    public GameObject bubbleEmitter;
+
+    void Start()
+    {
+        gameState = new GameState.LearnEnvironment(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameState.Update(this);
+        gameState = gameState.GetNext();
+    }
+}
+
+public abstract class GameState
+{
+    private GameState()
+    {
+    }
+
+    public abstract void Update(GameManager target);
+
+    public virtual GameState GetNext()
+    {
+        return this;
+    }
+
+    public sealed class LearnEnvironment : GameState
+    {
+        private ARPlaneManager arPlaneManager;
+        private GameManager manager;
+        private ARPlane gamePlane;
+
+        public LearnEnvironment(GameManager manager)
+        {
+            this.manager = manager;
+            arPlaneManager = manager.arPlaneManager;
+        }
+
+        public override void Update(GameManager target)
+        {
+            var mainPlane = Iterate(arPlaneManager).Where(plane => plane.alignment == PlaneAlignment.HorizontalUp)
+                .OrderBy(plane => plane.size.magnitude)
+                .FirstOrDefault();
+            if (mainPlane != null)
+            {
+                Debug.Log($"Game plane is: {mainPlane.center}, {mainPlane.size.magnitude}, {mainPlane.normal}");
+                Debug.DrawRay(mainPlane.center, mainPlane.normal, Color.magenta, 20, true);
+                if (mainPlane.size.magnitude > manager.areaThreshold)
+                {
+                    gamePlane = mainPlane;
+                    target.textField.text = "Game plane set!";
+                    manager.bubbleEmitter.transform.position = gamePlane.center;
+                    manager.bubbleEmitter.SetActive(true);
+                }
+            }
+            else
+            {
+                target.textField.text = "Scan a plane to play";
+            }
+        }
+
+        public override GameState GetNext()
+        {
+            if (gamePlane == null)
+            {
+                return this;
+            }
+            else
+            {
+                return new LocatePlayer(manager);
+            }
+        }
+    }
+
+    public sealed class LocatePlayer : GameState
+    {
+        private readonly GameManager manager;
+
+        public LocatePlayer(GameManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public override void Update(GameManager target)
+        {
+            manager.textField.text = "Looking for player";
+            Debug.DrawRay(manager.bubbleEmitter.transform.position, Vector3.up);
+        }
+    }
+
+    public sealed class PlayLevel : GameState
+    {
+        public LevelSettings LevelSettings { get; }
+
+        public PlayLevel(LevelSettings levelSettings)
+        {
+            this.LevelSettings = levelSettings;
+        }
+
+        public override void Update(GameManager target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class LevelComplete : GameState
+    {
+        public int Score { get; }
+
+        public LevelComplete(int score)
+        {
+            Score = score;
+        }
+
+        public override void Update(GameManager target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    static IEnumerable<ARPlane> Iterate(ARPlaneManager planeManager)
+    {
+        var iterator = planeManager.trackables.GetEnumerator();
+        while (iterator.MoveNext())
+        {
+            yield return iterator.Current;
+        }
+    }
+}
+
+public sealed class LevelSettings
+{
+}
--- Assets/SingleHandTracking/AR HandTracking/AR HandProvider/ARHand.cs
+++ Assets/SingleHandTracking/AR HandTracking/AR HandProvider/ARHand.cs
@@ -1,54 +1,88 @@
 using UnityEngine;
 using MediapipeHandTracking;
 
-public class ARHand {
+public class ARHand
+{
     private Vector3[] landmarks, landmarksCP = default;
-    public float currentDepth = 0f;
+    public float currentDepth = 0.4f;
     private Camera cam;
+    [SerializeField] private int depthDivisor = 80;
+    [SerializeField] public float debounceDistance = 0.02f;
 
-    public ARHand() {
+    public ARHand()
+    {
         landmarks = new Vector3[21];
         cam = Camera.main;
     }
 
-    private ARHand(Vector3[] landmarks) {
+    private ARHand(Vector3[] landmarks)
+    {
         this.landmarks = landmarks;
     }
 
-    public void ParseFrom(float[] arr, float c) {
+    public void ParseFrom(float[] arr, float c)
+    {
         if (null == arr || arr.Length < 63) return;
         //độ sâu của điểm ở cổ tay
 
-        for (int i = 0; i < 21; i++) {
+        for (int i = 0; i < 21; i++)
+        {
             float xScreen = Screen.width * ((arr[i * 3 + 1] - 0.5f * (1 - c)) / c);
             float yScreen = Screen.height * (arr[i * 3]);
-            this.landmarks[i] = cam.ScreenToWorldPoint(new Vector3(xScreen, yScreen, arr[i * 3 + 2] / 80 + 0.4f));
+            this.landmarks[i] =
+                cam.ScreenToWorldPoint(new Vector3(
+                    xScreen,
+                    yScreen,
+                    arr[i * 3 + 2] / depthDivisor + currentDepth)
+                );
         }
 
-        if (landmarksCP == default) {
+        if (landmarksCP == default)
+        {
             landmarksCP = new Vector3[21];
-            landmarksCP = (Vector3[])landmarks.Clone();
-        } else {
-            // nễu bị rung giữ nguyên landmark cũ
-            if (isVibrate(0.02f)) {
-                landmarks = (Vector3[])landmarksCP.Clone();
-            } else { // lưu lại landmark khi không bị rung
-                landmarksCP = (Vector3[])landmarks.Clone();
+            landmarksCP = (Vector3[]) landmarks.Clone();
+        }
+        else
+        {
+            // This is debounces the updates: the landmarks are only updated if any had moved more than a set distance
+            if (isVibrate(debounceDistance))
+            {
+                landmarks = (Vector3[]) landmarksCP.Clone();
+            }
+            else
+            {
+                // lưu lại landmark khi không bị rung
+                landmarksCP = (Vector3[]) landmarks.Clone();
             }
         }
     }
 
-    public bool isVibrate(float deltaVibrate) {
-        for (int i = 0; i < 21; i++) {
+    public bool isVibrate(float deltaVibrate)
+    {
+        for (int i = 0; i < 21; i++)
+        {
             if (Vector3.Distance(landmarksCP[i], landmarks[i]) > deltaVibrate) return false;
         }
+
         return true;
     }
 
     public Vector3 GetLandmark(int index) => this.landmarks[index];
     public Vector3[] GetLandmarks() => this.landmarks;
-    public ARHand Clone() {
-        return new ARHand((Vector3[])landmarks.Clone());
+
+    public ARHand Clone()
+    {
+        return new ARHand((Vector3[]) landmarks.Clone());
     }
 
+    enum HandJoints : int
+    {
+        Wrist = 0,
+        ThumbMCP = 2,
+        ThumbTip = 4,
+        IndexFingerMCP = 5,
+        IndexFingerTip = 8,
+        PinkyMCP = 17,
+        PinkyTip = 20
+    }
 }
